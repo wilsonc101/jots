8cd441ffc350bf9c85037c4ec03b7dc13d21236f
Chris <wilsonc101@gmail.com>
2020-06-22 23:18:01 +0100

    decorater on protected web views, tests still broken

--- webapp/private_api_endpoints.py
+++ webapp/private_api_endpoints.py
@@ -37,13 +37,23 @@ def protected_view(func):
       # Exceptions as 'passed' as they will be picked up by app check
       user_obj = None
       user_obj = jots.pyauth.user.user(email_address=requester_id, db=DB_CON)
-      _check_group_permission("admin", user_obj.properties.userId)
     except jots.pyauth.user.UserNotFound:
       pass
     except jots.pyauth.user.InputError:
       pass
 
-    if not user_obj:
+    if user_obj:
+      # If user object was created, check user is admin
+      try:
+        group = jots.pyauth.group.group(group_name="admin", db=DB_CON)
+      except jots.pyauth.group.GroupNotFound:
+        raise error_handlers.InvalidUsage("group not found", status_code=400)
+
+      if user_obj.properties.userId not in group.properties.members:
+        raise error_handlers.InvalidUsage("access denied", status_code=403)
+
+    else:
+      # No user object found, check if it's an app that's authing
       try:
         app_obj = jots.pyauth.app.app(app_name=requester_id, db=DB_CON)
       except jots.pyauth.app.AppNotFound:
@@ -53,24 +63,6 @@ def protected_view(func):
 
     return func(*args, **kwargs)
   return wrapper
-
-
-def _check_group_permission(group_name, user_id):
-  # Allow the use of a mock DB during testing
-  if app.config['TESTING']:
-    DB_CON = app.config['TEST_DB']
-  else:
-    DB_CON = None
-
-  try:
-    group = jots.pyauth.group.group(group_name=group_name, db=DB_CON)
-  except jots.pyauth.group.GroupNotFound:
-    raise error_handlers.InvalidUsage("group not found", status_code=400)
-
-  if user_id not in group.properties.members:
-    raise error_handlers.InvalidUsage("access denied", status_code=403)
-
-  return True
 
 
 @app.route('/api')
@@ -120,20 +112,13 @@ def api_findgroups():
 
 @app.route('/api/v1/groups/new', methods=['POST'])
 @jwt_required
+@protected_view
 def api_newgroup():
   # Allow the use of a mock DB during testing
   if app.config['TESTING']:
     DB_CON = app.config['TEST_DB']
   else:
     DB_CON = None
-
-  username = get_jwt_identity()
-  try:
-    user = jots.pyauth.user.user(email_address=username, db=DB_CON)
-  except jots.pyauth.user.UserNotFound:
-    raise error_handlers.InvalidUsage("invalid user", status=403)
-
-  _check_group_permission("admin", user.properties.userId)
 
   # Reject non-JSON payload
   if not request.json:
@@ -153,20 +138,13 @@ def api_newgroup():
 
 @app.route('/api/v1/groups/delete', methods=['POST'])
 @jwt_required
+@protected_view
 def api_deletegroup():
   # Allow the use of a mock DB during testing
   if app.config['TESTING']:
     DB_CON = app.config['TEST_DB']
   else:
     DB_CON = None
-
-  username = get_jwt_identity()
-  try:
-    user = jots.pyauth.user.user(email_address=username, db=DB_CON)
-  except jots.pyauth.user.UserNotFound:
-    raise error_handlers.InvalidUsage("invalid user", status=403)
-
-  _check_group_permission("admin", user.properties.userId)
 
   # Reject non-JSON payload
   if not request.json:
